0d7046b1e0fd2d6699c8338585575a4656495e7f
jannafierst <jfierst@fiu.edu>
2025-06-19 10:37:51 -0400

    Update run_slim.sh

--- Stochastic/run_slim.sh
+++ Stochastic/run_slim.sh
@@ -5,7 +5,7 @@ POP=1000 # Population size
 TE=4 # Number of initial TEs
 RECOMB=1e-2 # Recombination rate
 OUTCROSS=1.0 # Outcrossing rate
-MODEL=0 # Model of TE regulation, 0=Compete and 1=Parasite
+MODEL=0 # Model of TE regulation, 0=Symmetric and 1=Intrinsic
 
 for (( i=0; i<$ITERATE; i++ ))
 do
