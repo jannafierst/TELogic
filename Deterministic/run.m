dc1b9f97589153024bbd2313daffd5d80f5fbf87
jannafierst <jfierst@fiu.edu>
2025-06-19 13:36:46 -0400

    Update run.m

--- Deterministic/run.m
+++ Deterministic/run.m
@@ -1,6 +1,6 @@
-%intrinsic(u1, u2, v, a1, a2, s, m12 ,m21, N1_0, N2_0)
-%intrinsic(0.01, 0.01, 0.001, 1, 0, -0.001, 0.01, 0.01, 20, 20) % uncomment
-%to run intrinsic TE regulation model
+%attenuating(u1, u2, v, a1, a2, s, m12 ,m21, N1_0, N2_0)
+%attenuating(0.01, 0.01, 0.001, 1, 0, -0.001, 0.01, 0.01, 20, 20) % uncomment
+%to run self-attenuating TE regulation model
 
 %symmetric(u1, u2, v, a1, a2, s, m12, m21, N1_0, N2_0)
 %symmetric(0.01, 0.01, 0.001, 1, 0, -0.001, 0.01, 0.01, 2, 2) % uncomment to
