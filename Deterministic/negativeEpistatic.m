09854fd311fe78f54f9ecfe4f75402e1a3b39bb4
jannafierst <jfierst@fiu.edu>
2026-06-02 16:30:21 -0400

    Update negativeEpistatic.m

--- Deterministic/negativeEpistatic.m
+++ Deterministic/negativeEpistatic.m
@@ -8,13 +8,13 @@ function [u, v, a, b, N_0] = charlesworth(u, v, a, b, N_0)
 %a, b determine the curvature of the fitness function
 %N_0 = initial number of TE
 
-n = 20000;
+g = 20000;
 n1 = N_0;
 
 A = [n1];
 N1 = n1;
 fileID = fopen('negativeEpistatic.txt','w');
-for i=1:1:n % here, N is number of generations
+for i=1:1:g % here, g is number of generations
     Sn = -a - b*N1;
     delta_n1 = N1 * (Sn + u - v); % assuming a Poisson bc variance is the mean (N1)
     n1 = N1 + delta_n1;
